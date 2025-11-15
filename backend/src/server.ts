dd183d9a04f2acf085d3a1e1670897bbda3daabf
Shubham <144575875+Shumbham-Gupta@users.noreply.github.com>
2025-11-15 21:13:11 +0530

    Update CORS middleware configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,13 @@ const app: Express = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://todos-app-frontend-my1d.onrender.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
